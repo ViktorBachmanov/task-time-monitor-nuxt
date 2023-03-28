202d8bf4c0159f6435540605c32b4b6734d0f02e
Vikt0r <home@home.ru>
2023-03-28 18:11:07 +0300

    Test server/api

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup>
+function testApi() {
+  fetch('/api/hello')
+    .then(res => {
+      console.log('Response has arrived')
+    })
+}
+</script>
+
 <template>
   <div>
+    <button @click="testApi">Test API</button>
     <NuxtWelcome />
   </div>
 </template>
