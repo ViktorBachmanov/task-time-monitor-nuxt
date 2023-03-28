9a5f79eb6dd99329271c315a068025703d6d86b7
Vikt0r <home@home.ru>
2023-03-28 19:30:14 +0300

    Install Mysql2 and successfully test it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    // apiSecret: "123",
+    databaseHost: "",
+    databaseUser: "",
+    databasePassword: "",
+    database: "",
+    // Keys within public are also exposed client-side
+    public: {
+      // apiBase: '/api'
+    },
+  },
+});
--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,19 @@
-export default defineEventHandler((event) => {
+import * as mysql from "mysql2/promise";
+
+export default defineEventHandler(async (event) => {
+  const runtimeConfig = useRuntimeConfig();
+
+  const connection = await mysql.createConnection({
+    host: runtimeConfig.databaseHost,
+    user: runtimeConfig.databaseUser,
+    password: runtimeConfig.databasePassword,
+    database: runtimeConfig.database,
+  });
+  await connection.execute("INSERT INTO `task` (id) VALUES (1)");
+  const [rows, fields] = await connection.execute("SELECT * FROM `task`");
   return {
     api: "works!",
+    rows,
+    fields,
   };
 });
