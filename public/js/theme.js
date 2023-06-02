86627eb9f0a6345e3c1c632f5e05b9a460f92f9a
Vikt0r <home@home.ru>
2023-06-02 21:31:33 +0300

    Add 'prefers-color-scheme' media query

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,16 +1,11 @@
 const themeStorageKey = "theme_ttm";
 
-if (localStorage[themeStorageKey] === "dark") {
+if (
+  localStorage[themeStorageKey] === "dark" ||
+  (!(themeStorageKey in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
   document.documentElement.classList.add("dark");
 } else {
   document.documentElement.classList.remove("dark");
 }
-
-// if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-//   const faviconElem: HTMLImageElement | null = document.getElementById("favicon");
-//   if(faviconElem) {
-//     faviconElem.href = "/favicon-dark.png";
-//   }
-// }
-
-// export { themeStorageKey };
