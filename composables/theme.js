eecabf56dd6fe4988357b30f389ccb1b4e04f792
Vikt0r <home@home.ru>
2023-06-03 08:09:36 +0300

    Add storing in localStorage to theme composable

--- composables/theme.js
+++ composables/theme.js
@@ -1,19 +1,20 @@
 export function useTheme() {
   const theme = ref("dark");
 
+  const themeStorageKey = "theme_ttm";
+
   const toggleTheme = () => {
     switch (theme.value) {
+      default:
       case "light":
         theme.value = "dark";
         document.documentElement.classList.add("dark");
+        localStorage[themeStorageKey] = "dark";
         break;
       case "dark":
         theme.value = "light";
         document.documentElement.classList.remove("dark");
-        break;
-      default:
-        theme.value = "dark";
-        document.documentElement.classList.add("dark");
+        localStorage[themeStorageKey] = "light";
         break;
     }
   };
