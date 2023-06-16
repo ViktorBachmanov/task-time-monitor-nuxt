d10475bbd0b1c811a3a292478190928f5a1a6ebe
Vikt0r <home@home.ru>
2023-06-16 10:24:39 +0300

    Nearly is done

--- components/AddItemV.vue
+++ components/AddItemV.vue
@@ -13,37 +13,11 @@ const props = defineProps({
 
 const emit = defineEmits(['added'])
 
-const isActive = ref(false)
-const isPending = ref(false)
+const dialog = ref(false)
+const pending = ref(false)
 const notification = ref(null)
 
 const itemName = ref('')
-
-async function handleAdding() {
-  isPending.value = true
-
-  const { data, error } = await useFetch(props.url, {
-    method: 'POST',
-    body: {
-      itemName: itemName.value,
-      ...props.payload,
-    }
-  })
-
-  itemName.value = ''
-
-  if(data.value) {
-    notification.value.success(props.successMessage)
-    emit('added', data.value.createdItem.id)
-  }
-  else if(error.value) {
-    notification.value.error('Ошибка')
-  }
-
-
-  isActive.value = false
-  isPending.value = false
-}
 
 const rules = [
   value => {
@@ -55,12 +29,35 @@ const rules = [
 
 const isFormValid = ref(null)
 
-function handleSubmit() {
-  console.log('isFormValid: ', isFormValid.value)
 
+async function handleSubmit() {
   if(!isFormValid.value) {
-    alert('not valid')
+    return
   }
+
+  pending.value = true
+
+  const { data, error } = await useFetch(props.url, {
+    method: 'POST',
+    body: {
+      itemName: itemName.value,
+      ...props.payload,
+    }
+  })
+
+
+  if(data.value) {
+    notification.value.success(props.successMessage)
+    emit('added', data.value.createdItem.id)
+  }
+  else if(error.value) {
+    notification.value.error('Ошибка')
+  }
+
+  pending.value = false
+  dialog.value = false
+  itemName.value = ''
+
 }
 </script>
 
@@ -71,36 +68,52 @@ function handleSubmit() {
     icon
     rounded="lg"
     variant="tonal"
-    @click="isActive = true"
+    @click="dialog = true"
   >
     <v-icon icon="mdi-plus" size="x-large"></v-icon>
   </v-btn>
 
   <v-dialog
-    v-model="isActive"
+    v-model="dialog"
     width="auto"
   >
-    <v-card>
-      <v-card-text>
-        {{ header }}
-      </v-card-text>
-
+    <v-card 
+      style="padding: 0 0.5em 0.5em"
+      :title="header"
+    >
       <v-form 
         @submit.prevent="handleSubmit"
         v-model="isFormValid"
+        style="margin: 1.5em 0.5em 0.5em"
       >
         <v-text-field 
           label="Наименование" 
           variant="outlined"
           v-model="itemName"
           :rules="rules"
-          validate-on="blur"
           autofocus
+          style="margin-bottom: 1em"
         >
         </v-text-field>
 
-        <v-btn color="primary" type="submit">Ok</v-btn>
-        <v-btn color="primary"  @click="isActive = false">Cancel</v-btn>
+        <div style="display: flex; justify-content: center; column-gap: 1em;">
+          <v-btn 
+            color="primary" 
+            type="submit" 
+            :loading="pending"
+          >
+            Ok
+          </v-btn>
+
+          <v-btn 
+            color="primary"  
+            @click="dialog = false" 
+            :loading="pending"
+          >
+            Cancel
+          </v-btn>
+        </div>
+
       </v-form>
 
     </v-card>
@@ -110,10 +123,3 @@ function handleSubmit() {
   <TheNotification ref="notification" />
 </template>
 
-
-<style lang="scss" scoped>
-button.disabled {
-  opacity: 0.5;
-  pointer-events: none;
-}
-</style>
